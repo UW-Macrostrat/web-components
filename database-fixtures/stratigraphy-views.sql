1351ea2e74ba1f7a3a60ea78f3835f9fa5735b3e
Daven Quinn <dev@davenquinn.com>
2018-08-20 16:48:22 -0500

    Added new strat views

--- database-fixtures/stratigraphy-views.sql
+++ database-fixtures/stratigraphy-views.sql
@@ -40,43 +40,37 @@ JOIN section.section s
 
 CREATE OR REPLACE VIEW section.generalized_section_surface AS
 WITH l AS (
-SELECT
-	l.id,
-	b.locality section,
-	f.color facies_color,
-	l.section original_section,
-	l.bottom original_height,
-	l.bottom+section.generalized_offset(l.section) bottom,
-	l.covered,
-	l.lithology,
-	l.surface,
-  coalesce(definite_boundary, true) definite_boundary,
-  coalesce(v.pattern, l.lithology) pattern,
-	l.fgdc_pattern::text,
-	coalesce(l.schematic, false) schematic,
-	l.grainsize,
-	l.fill_pattern,
-	l.facies
-FROM section.section_lithology l
-JOIN section.generalized_breaks b
-  ON l.section = b.section
-LEFT JOIN section.facies f
-  ON l.facies = f.id
-LEFT JOIN section.lithology v
-  ON l.lithology = v.id
-WHERE b.lower_height <= l.bottom
-  AND l.bottom < b.upper_height
+  SELECT * FROM section.section_surface_data
 )
 SELECT
-	l.*,
-  coalesce(
-    lead(l.bottom) OVER (PARTITION BY l.section ORDER BY l.bottom),
-    s.end + section.generalized_offset(l.original_section)
-  )::float top,
-  t.tree
+  l.id,
+  l.facies,
+  s.location section,
+  l.section original_section,
+  l.bottom original_bottom,
+  l.top original_top,
+  l.bottom+section.generalized_offset(l.section) bottom,
+  l.top+section.generalized_offset(l.section) top,
+  l.facies_color,
+  l.lithology,
+  l.covered,
+  l.flooding_surface_order,
+  l.surface,
+  l.note,
+  l.definite_boundary,
+  l.pattern,
+  l.schematic,
+  l.fgdc_pattern,
+  l.tree,
+  l.grainsize,
+  l.surface_type_1,
+  l.surface_type_2,
+  l.fill_pattern
 FROM l
-JOIN section.lithology_tree t
-  ON l.lithology = t.id
+JOIN section.generalized_breaks b
+  ON l.section = b.section
 JOIN section.section s
-  ON s.id = l.original_section
-ORDER BY section, bottom;
+  ON s.id = l.section
+WHERE b.lower_height <= l.bottom
+  AND l.bottom < b.upper_height
+ORDER BY s.location, bottom;
