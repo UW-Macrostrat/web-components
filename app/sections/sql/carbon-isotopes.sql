bcfaaaa2dddeb3a9ed2682c57c5e40ae7a38c5d7
Daven Quinn <dev@davenquinn.com>
2019-04-28 14:39:51 -0500

    Updated carbon isotope display functions

--- app/sections/sql/carbon-isotopes.sql
+++ app/sections/sql/carbon-isotopes.sql
@@ -1,53 +1,24 @@
-WITH isotopes AS (
+WITH a AS (
 SELECT
-  analysis,
-  avg(delta13c) avg_delta13c,
-  stddev(delta13c) std_delta13c,
-  avg(delta18o) avg_delta18o,
-  stddev(delta18o) std_delta18o,
-  count(*) AS n
-FROM carbon_isotopes.analysis_peak p
-JOIN carbon_isotopes.analysis a USING (analysis)
-WHERE peak_type = 'data'
-  AND NOT a.is_standard
-GROUP BY (analysis)
-),
-v AS (
-SELECT DISTINCT ON (analysis)
-  isotopes.*,
-  sample_id,
-  height::numeric height,
-  section,
-  a.date
-FROM isotopes
-JOIN carbon_isotopes.analysis a USING (analysis)
-LEFT JOIN carbon_isotopes.analysis_failure f
-  ON a.sample_id = f.id
-  AND a.date = f.date
-WHERE failure_mode IS null
-  AND std_delta13c IS NOT null
-  AND n >= 3
-ORDER BY analysis, height
-),
-z AS (
-SELECT
-  section || '-' || height::text id,
-  sum(avg_delta13c*n)/sum(n) avg_delta13c,
-  sum(avg_delta18o*n)/sum(n) avg_delta18o,
-  sqrt(sum(power(std_delta13c, 2)*n)/sum(n)) std_delta13c,
-  sqrt(sum(power(std_delta18o, 2)*n)/sum(n)) std_delta18o,
+  sample_id id,
+  d.avg_delta13c,
+  d.avg_delta18o,
+  d.std_delta13c,
+  d.std_delta18o,
   section,
   height orig_height,
-  sum(n) n,
+  n,
   (section.normalized_height(section::text, height::numeric)).*,
   CASE WHEN (section = 'J' AND height < 14.3) THEN
     false
   ELSE
     true
   END AS in_zebra_nappe
-FROM v
-GROUP BY (section, height)
+FROM carbon_isotopes.analysis_data d
+JOIN carbon_isotopes.corrected_data c
+  ON d.analysis_id = c.analysis_id
+WHERE failure_mode IS null
 )
-SELECT * FROM z
-WHERE height IS NOT null
-ORDER BY height;
+SELECT * FROM a
+WHERE height IS NOT null;
+
