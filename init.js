0f42941da0a3b2445a6d6cd20c480999c0c852c2
Daven Quinn <dev@davenquinn.com>
2019-11-06 15:43:51 -0600

    Added color aliases and remove outdated blueprintjs/icons from init

--- init.js
+++ init.js
@@ -6,7 +6,6 @@
 import '@babel/polyfill' // this seems suspect
 import {FocusStyleManager} from '@blueprintjs/core'
 import '@blueprintjs/core/lib/css/blueprint.css'
-import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import './lib/index.css'
 
 FocusStyleManager.onlyShowFocusOnTabs();
