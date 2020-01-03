b8678d048eb812d438684d0389325f159a3f26ae
Daven Quinn <dev@davenquinn.com>
2020-01-03 17:43:31 -0500

    Slightly better compilation of typings

--- packages/map-components/src/map-components.ts
+++ packages/map-components/src/map-components.ts
@@ -1,7 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export {MapContext} from './context'
 
 export default class DummyClass {
 
