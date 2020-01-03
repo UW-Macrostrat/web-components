25edadc4997b1dd544d4f2425af8b9265e5a83f6
Daven Quinn <dev@davenquinn.com>
2020-01-03 03:39:51 -0500

    Updated patterns

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ import {
   APIProvider,
   APIResultView
 } from './ui-components';
+import {
+  GeologicPatternProvider
+} from './column-components'
 import Column, {IUnit} from './column'
+import patterns from '../geologic-patterns/*.png'
 
 const renderResults = (data: Array<IUnit>)=> {
   return h(Column, {data});
@@ -12,15 +16,19 @@ const renderResults = (data: Array<IUnit>)=> {
 const MainView = => {
   return h(APIResultView, {
     route: "/units",
-    params: {all: true, col_id: 495, long: true}
+    params: {all: true, col_id: 495, response: 'long'}
   }, renderResults);
 };
 
+const resolvePattern = (id)=>patterns[id]
+
 const App = => {
-  return h(APIProvider, {
-    baseURL: "https://dev.macrostrat.org/api/v2",
-    unwrapResponse: (res)=>res.success.data
-  }, h(MainView));
-};
+  return h(GeologicPatternProvider, {resolvePattern}, (
+    h(APIProvider, {
+      baseURL: "https://dev.macrostrat.org/api/v2",
+      unwrapResponse: (res)=>res.success.data
+    }, h(MainView))
+  )
+}
 
 export default App
--- src/units/index.ts
+++ src/units/index.ts
@@ -2,34 +2,46 @@ import h from '@macrostrat/hyper'
 import {useContext} from 'react'
 import {
   LithologyColumn,
+  LithologySymbolDefs
   ColumnContext,
   ColumnLayoutContext
+  defaultResolveID
+  useUUID
 } from '../column-components'
 import {IUnit} from './types'
 
 interface UnitProps {
   division: IUnit
+  resolveID(IUnit): string
 }
 
 const Unit = (props: UnitProps)=>{
-  const {division: d} = props
+  const {division: d, resolveID} = props
   const {scale} = useContext(ColumnContext)
   const {width} = useContext(ColumnLayoutContext)
 
   const y = scale(d.t_age)
   const height = scale(d.b_age)-y
 
+  console.log(d.lith)
+
   return h("rect.unit", {x: 0, y, width, height, fill: "#aaa"})
 }
 
 const UnitBoxes = (props)=>{
   const {divisions} = useContext(ColumnContext)
+  const resolveID = defaultResolveID
+  const UUID = useUUID()
 
-  return h('g.divisions', divisions.map(div =>{
-    return h(Unit, {
-      division: div
-    })
-  }))
+  return h('g.divisions', [
+    h(LithologySymbolDefs, {resolveID, UUID})
+    h('g', divisions.map(div =>{
+      return h(Unit, {
+        division: div
+        resolveID
+      })
+    }))
+  ])
 }
 
 const UnitsColumn = (props)=>{
