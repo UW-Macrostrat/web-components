5dfd95824929dbeb45705e675c77eae3692776b4
Daven Quinn <dev@davenquinn.com>
2020-01-03 16:43:05 -0500

    Updated packaging strategy

--- packages/map-components/src/map-components.ts
+++ packages/map-components/src/map-components.ts
@@ -1,6 +1,8 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
+export {MapContext} from './context'
+
 export default class DummyClass {
 
 }
