949536166d5ecc98f2877b80db5442668d638399
Daven Quinn <dev@davenquinn.com>
2020-02-29 03:37:27 -0600

    Updated column context

--- packages/column-components/src/context/settings.ts
+++ packages/column-components/src/context/settings.ts
@@ -8,50 +8,67 @@ import h from 'react-hyperscript';
 import {createContext, useState, useContext} from 'react';
 import update, {Spec} from 'immutability-helper';
 import LocalStorage from '../util/storage';
-import T from 'prop-types';
-
-const SettingsUpdateContext = createContext(function(){});
-const SettingsContext = createContext({});
 
 // Could rework this to use `constate` or similar
 
-const SettingsProvider = function(props){
-  /*
-  A settings provider that can be used with LocalStorage
+function createSettingsContext<T extends {}>(defaultValue: T) {
+  /** Function that creates a context with an updateable settings object
   */
-  let {storageID, children, ...defaultSettings} = props;
-  // Update from local storage
-  let storage = null;
-  if (storageID != null) {
-    // Merge initial options if set
-    storage = new LocalStorage(storageID);
-    const v = storage.get() || {};
-    console.log("Loading from local storage", v);
-    defaultSettings = update(defaultSettings, {$merge: v});
+  const Context = createContext<T>(defaultValue);
+
+  type SettingsUpdateSpec = Spec<T>
+  type Updater = (spec: SettingsUpdateSpec) => void
+  const UpdateContext = createContext<Updater|null>(null);
+  interface SettingsProviderProps {
+    children: React.ReactNode,
+    storageID?: string
   }
 
-  const [settings, setState] = useState(defaultSettings);
-  const updateState = function(spec: Spec){
-    const newSettings = update(settings, spec);
-    setState(newSettings);
-    if (storage != null) { return storage.set(newSettings); }
+  const Provider = function(props: SettingsProviderProps & T){
+    /*
+    A settings provider that can be used with LocalStorage
+    */
+    let {storageID, children, ...defaultSettings} = props;
+    // Update from local storage
+    let storage = null;
+    if (storageID != null) {
+      // Merge initial options if set
+      storage = new LocalStorage(storageID);
+      const v = storage.get() || {};
+      console.log("Loading from local storage", v);
+      defaultSettings = update(defaultSettings, {$merge: v});
+    }
+
+    const [settings, setState] = useState<T>(defaultSettings);
+    const updateState = function(spec: SettingsUpdateSpec){
+      const newSettings = update(settings, spec);
+      setState(newSettings);
+      if (storage != null) { return storage.set(newSettings); }
+    };
+
+    return h(Context.Provider, {value: settings}, [
+      h(UpdateContext.Provider, {value: updateState}, children)
+    ]);
   };
 
-  return h(SettingsContext.Provider, {value: settings}, [
-    h(SettingsUpdateContext.Provider, {value: updateState}, children)
-  ]);
-};
+  const useValue = () => useContext(Context);
 
-SettingsProvider.propTypes = {
-  storageID: T.string
-};
+  const useUpdater = (key: string|undefined) =>{
+    const updater = useContext(UpdateContext)
+    if (key == null) return updater
+    return (spec)=>updater({[key]: spec})
+  }
 
-const useSettings = () => useContext(SettingsContext);
+  return [Provider, useValue, useUpdater]
+}
 
+const [SettingsProvider, useSettings, useSettingsUpdater] = createSettingsContext<object>({})
+
+// Deprecated: this is kind of confusing
 const updateSettings = function(func){
   // Update settings using `immutability-helper` semantics
-  const updater = useContext(SettingsUpdateContext);
+  const updater = useSettingsUpdater();
   return function() { return updater(func(...arguments)); };
 };
 
-export {SettingsProvider, SettingsContext, useSettings, updateSettings};
+export {SettingsProvider, useSettings, updateSettings};
