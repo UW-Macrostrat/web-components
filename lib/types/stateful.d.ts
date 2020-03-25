06487fb9cde9608da92b7029f0ad2c751df2c4e1
Daven Quinn <dev@davenquinn.com>
2020-03-25 01:13:19 -0500

    Removed lib directory from tracking

--- lib/types/stateful.d.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from 'react';
-import { Spec } from 'immutability-helper';
-declare class StatefulComponent<Props, State> extends Component<Props, State> {
-    constructor(props: Props);
-    updateState(spec: Spec<State>): void;
-}
-export { StatefulComponent };
