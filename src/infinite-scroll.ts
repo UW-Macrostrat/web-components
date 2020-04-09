d022dfab34bf077c982fccc6f50b6d1f023cdcf7
Daven Quinn <dev@davenquinn.com>
2020-04-09 12:37:29 -0500

    Start of reducer-based infinite scroll

--- src/infinite-scroll.ts
+++ src/infinite-scroll.ts
@@ -1,21 +1,19 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import InfiniteScroll from 'react-infinite-scroller';
 import h from 'react-hyperscript';
+import update, {Spec} from 'immutability-helper'
+import {useReducer} from 'react'
+
 import {APIView, APIResultProps, useAPIActions} from "./api";
 import {useImmutableState} from './util';
 import {useAsyncEffect} from './util';
 
-interface ScrollState<T> {
+interface ScrollState<T=object> {
   items: T[],
   scrollParams: QueryParams,
   count: number,
   error?: any,
-  hasMore: boolean
+  hasMore: boolean,
+  isLoadingPage: number|null
 }
 
 interface InfiniteScrollProps<T> extends APIResultProps<T> {
@@ -25,13 +23,34 @@ interface InfiniteScrollProps<T> extends APIResultProps<T> {
   hasMore(s: ScrollState<T>, res: T): boolean,
 }
 
+type UpdateState<T> = {type: 'update-state', spec: Spec<ScrollState<T>>}
+
+type ScrollAction<T> =
+  | UpdateState<T>
+
+type Reducer<T> = (state: ScrollState<T>, action: ScrollAction<T>)=>ScrollState<T>
+
+const infiniteScrollReducer = function<T>(state: ScrollState<T>, action: ScrollAction<T>) {
+  switch (action.type) {
+  case "update-state":
+    return update(state, action.spec)
+  }
+}
+
 const InfiniteScrollView = function<T>(props: InfiniteScrollProps<T>){
   /*
   A container for cursor-based pagination. This is built for
   the GeoDeepDive API right now, but it can likely be generalized
   for other uses.
   */
-  const {route, params, opts, children, placeholder, className} = props;
+  const {
+    route,
+    params,
+    opts,
+    children,
+    placeholder,
+    className
+  } = props;
   const {get} = useAPIActions();
   const {getCount, getNextParams, getItems, hasMore} = props
 
@@ -41,25 +60,30 @@ const InfiniteScrollView = function<T>(props: InfiniteScrollProps<T>){
     count: null,
     error: null,
     hasMore: true,
+    isLoadingPage: null
   }
 
-  const [state, updateState] = useImmutableState(initialState);
+  const [state, dispatch] = useReducer<Reducer<T>>(infiniteScrollReducer, initialState)
+
 
   const parseResponse = (res: T, page: number)=>{
     const itemVals = getItems(res)
     const ival = page == 0 ? {$set: itemVals} : {$push: itemVals}
     const nextLength = state.items.length + itemVals.length
     const count = getCount(res)
-    updateState({
+    console.log(`Finished loading page ${page}`)
+    dispatch({type: "update-state", spec: {
       items: ival,
       scrollParams: {$set: {...params, page: page+1}},
       count: {$set: count},
-      hasMore: {$set: hasMore(state, res) && itemVals.length > 0 && nextLength <= count}
-    });
+      hasMore: {$set: hasMore(state, res) && itemVals.length > 0 && nextLength <= count},
+      isLoadingPage: {$set: null}
+    }});
   }
 
   const loadNext = async function(page: number) {
     console.log("Loading page ", page)
+    dispatch({type: "update-state", spec: {isLoadingPage: {$set: page}}})
     const success = await get(route, state.scrollParams, opts)
     parseResponse(success, page)
   };
@@ -70,19 +94,20 @@ const InfiniteScrollView = function<T>(props: InfiniteScrollProps<T>){
     */
     // const success = await get(route, params, opts);
     // parseResponse(success, true)
-    updateState({$set: initialState})
+    if (state.items.length == 0 && state.isLoadingPage == null)
+    dispatch({type: 'update-state', spec: {$set: initialState}})
     //await loadNext(0)
   };
 
-  //loadInitialData()
   useAsyncEffect(loadInitialData, [props.route, props.params])
+  if (state == null) return null
 
   //useAsyncEffect(getInitialData, [route, params]);
 
   return h(InfiniteScroll, {
     pageStart: 0,
     loadMore: loadNext,
-    hasMore: state.hasMore,
+    hasMore: state.hasMore && state.isLoadingPage == null,
     loader:  state.items.length == 0 ? null : placeholder,
     useWindow: true,
     className
@@ -91,7 +116,7 @@ const InfiniteScrollView = function<T>(props: InfiniteScrollProps<T>){
       route,
       params: state.scrollParams,
       placeholder,
-      isLoading: state.hasMore,
+      isLoading: state.isLoadingPage != null,
     }, children)
   );
 };
