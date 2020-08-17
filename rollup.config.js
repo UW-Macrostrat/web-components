3bd764d64eda83a9f471e35fbea99294453f93ec
Daven Quinn <dev@davenquinn.com>
2020-08-17 04:18:48 -0500

    Simpler config (entry module in root dir)

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
 import pkg from "./package.json";
-import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
 import typescript from "@rollup/plugin-typescript";
 const deps = { ...pkg.dependencies, ...pkg.peerDependencies };
 
 //https://2ality.com/2017/02/babel-preset-env.html
 
-const extensions = [".js", ".ts", ".tsx", ".d.ts"];
-
 export default {
-  input: "src/index.ts", // our source file
+  input: "index.ts", // our source file
   preserveModules: true,
   output: [
     {
@@ -26,6 +23,6 @@ export default {
       modules: false,
       extract: "index.css"
     }),
-    typescript({})
+    typescript()
   ]
 };
