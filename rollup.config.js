a90a1ef1aa67705e48ddc4df9056591800c8cc38
Daven Quinn <dev@davenquinn.com>
2020-08-17 04:07:51 -0500

    Puts things in wrong folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,31 @@
 import pkg from "./package.json";
-import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
+import typescript from "@rollup/plugin-typescript";
 const deps = { ...pkg.dependencies, ...pkg.peerDependencies };
 
 //https://2ality.com/2017/02/babel-preset-env.html
 
-const extensions = [".js", ".ts", ".d.ts"];
+const extensions = [".js", ".ts", ".tsx", ".d.ts"];
 
 export default {
   input: "src/index.ts", // our source file
   preserveModules: true,
   output: [
     {
-      dir: pkg.main,
-      format: "cjs",
-      sourcemap: true,
-      entryFileNames: "[name].js"
-    },
-    {
-      dir: pkg.module,
+      dir: "lib",
       format: "esm",
-      sourcemap: true,
-      entryFileNames: "[name].js"
+      sourcemap: true
     }
   ],
   external: Object.keys(deps),
   plugins: [
-    resolve({ extensions, module: true }),
+    //    resolve({ extensions, module: true }),
     postcss({
       // postfix with .module.css etc. for css modules (DISABLED)
       modules: false,
       extract: "index.css"
     }),
-    babel({
-      extensions,
-      exclude: "node_modules/**"
-    })
+    typescript({})
   ]
 };
