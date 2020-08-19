884616fc26795a20a314cc109cecc33e5f07c1ed
Daven Quinn <dev@davenquinn.com>
2020-08-19 01:41:38 -0500

    Fix bundling error

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,34 @@
 import pkg from "./package.json";
 import postcss from "rollup-plugin-postcss";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
 const deps = { ...pkg.dependencies, ...pkg.peerDependencies };
 
 //https://2ality.com/2017/02/babel-preset-env.html
 
 export default {
-  input: "index.ts", // our source file
+  input: "src/index.ts", // our source file
   preserveModules: true,
   output: [
     {
-      dir: "lib",
+      dir: "lib/esm",
       format: "esm",
+      sourcemap: true
+    },
+    {
+      dir: "lib/cjs",
+      format: "cjs",
       sourcemap: true
     }
   ],
   external: Object.keys(deps),
   plugins: [
-    //    resolve({ extensions, module: true }),
+    resolve({ module: true }),
     postcss({
       // postfix with .module.css etc. for css modules (DISABLED)
       modules: false,
       extract: "index.css"
     }),
-    typescript()
+    typescript({ useTsconfigDeclarationDir: true })
   ]
 };
