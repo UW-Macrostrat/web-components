8ce1392cdcd129c57cc4b1252fbabeb4f3a75688
Daven Quinn <dev@davenquinn.com>
2020-08-21 02:32:05 -0500

    Small updates

---
+++ packages/timescale/README.md
@@ -0,0 +1,9 @@
+# React Geologic Timescale
+
+In the geosciences, we often need to represent our work against the long sweep
+of Earth history. This component provide a configurable time axis for all manner
+of visualizations.
+
+## Prior art
+
+- John Czaplewski's [initial implementation](http://bl.ocks.org/jczaplew/7546689) using D3.js
--- packages/timescale/src/index.ts
+++ packages/timescale/src/index.ts
@@ -1,5 +1,42 @@
 import h from "@macrostrat/hyper";
+import { defaultIntervals } from "./intervals";
 
-export function Timescale() {
-  return h("div.timescale", { style: { width: 300, height: 200 } });
+interface Interval {
+  nam: string;
+  lvl: number;
+  color: string;
+  lag: number;
+  eag: number;
 }
+
+interface TimescaleProps {
+  intervals?: Interval[];
+}
+
+function Timescale(props: TimescaleProps) {
+  /**
+   * A geologic timescale component for react.
+   *
+   * @remarks
+   * Nothing yet.
+   *
+   * @param intervals - Intervals
+   * @param width - Width of the timescale (optional)
+   *
+   */
+  const { intervals } = props;
+
+  const l1 = intervals.filter((d) => d.lvl == 1);
+
+  return h(
+    "div.timescale",
+    { style: { width: 800, height: 200 } },
+    l1.map((d) => {
+      return h("div.interval", h("span.interval-label", d.nam));
+    })
+  );
+}
+
+Timescale.defaultProps = { intervals: defaultIntervals };
+
+export { Timescale };
