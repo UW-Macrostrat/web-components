042fe0bd9eef61e365b7696308880391fc8786f6
Daven Quinn <dev@davenquinn.com>
2020-08-21 01:03:43 -0500

    A basic module

---
+++ packages/timescale/example/main.ts
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+import h from "@macrostrat/hyper";
+import { Timescale } from "../src";
+import "./main.styl";
+
+render(h(Timescale), document.getElementById("root"));
---
+++ packages/timescale/src/index.ts
@@ -0,0 +1,5 @@
+import h from "@macrostrat/hyper";
+
+export function Timescale() {
+  return h("div.timescale", { style: { width: 300, height: 200 } });
+}
