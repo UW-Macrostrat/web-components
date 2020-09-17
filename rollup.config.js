a00d8a4036e9bd651aa14f40be8c5d53e206496f
Daven Quinn <dev@davenquinn.com>
2020-09-17 05:21:21 -0500

    Better bundling of CSS

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import pkg from "./package.json";
 import babel from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
+import path from "path";
 const deps = { ...pkg.dependencies, ...pkg.peerDependencies };
 
 //https://2ality.com/2017/02/babel-preset-env.html
@@ -31,7 +32,8 @@ export default {
     postcss({
       // postfix with .module.css etc. for css modules (DISABLED)
       modules: false,
-      extract: "lib/index.css"
+      // CSS cannot be extracted outside of the bundle directory for rollup v2.
+      extract: "index.css"
     }),
     babel({
       extensions,
