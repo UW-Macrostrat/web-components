2bc897f6681c6d28dc766ef030448c21a402808d
Daven Quinn <dev@davenquinn.com>
2020-12-15 19:01:49 -0500

    Take only measurements with specific IDs

--- apps/zircons/map/layers.ts
+++ apps/zircons/map/layers.ts
@@ -1,38 +1,23 @@
-import { useAPIResult } from '@macrostrat/ui-components'
-import h from '@macrostrat/hyper'
-import { FeatureLayer } from '@macrostrat/map-components'
-import { feature } from 'topojson-client'
-
-function processTopoJSON(res) {
-  try {
-    const { data } = res.success
-    return feature(data, data.objects.output)
-  } catch (err) {
-    console.error(err)
-    return []
-  }
-}
+import h from "@macrostrat/hyper"
+import { useAPIResult } from "@macrostrat/ui-components"
+import { FeatureLayer } from "@macrostrat/map-components"
 
 const MeasurementsLayer = props => {
-  const geometry = useAPIResult(
-    '/measurements',
-    {
-      format: 'topojson',
-      measure_phase: 'zircon',
-      measurement: '207Pb-206Pb',
-      response: 'light',
-    },
-    processTopoJSON
-  )
-  if (geometry == null) return null
+  const res = useAPIResult("/measurements", {
+    format: "geojson",
+    measure_phase: "zircon",
+    measurement: "207Pb-206Pb",
+    response: "light",
+  })
+  if (res == null) return null
 
   return h(FeatureLayer, {
     useCanvas: false,
     style: {
-      fill: 'rgb(239, 180, 249)',
-      stroke: 'magenta',
+      fill: "rgb(239, 180, 249)",
+      stroke: "magenta",
     },
-    geometry,
+    features: res.features.filter(d => d.properties.unit_id != null),
   })
 }
 
