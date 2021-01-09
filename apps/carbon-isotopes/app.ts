e3c8f69a71ca09590376efbb152b0dd68e3aab30
Daven Quinn <dev@davenquinn.com>
2021-01-09 03:20:32 -0500

    Added a basic carbon isotopes app

---
+++ apps/carbon-isotopes/app.ts
@@ -0,0 +1,68 @@
+import {useState} from 'react'
+import h from '@macrostrat/hyper';
+import {
+  APIProvider,
+  APIResultView,
+  useAPIResult,
+  getQueryString,
+} from '@macrostrat/ui-components';
+import {
+  GeologicPatternProvider
+} from '@macrostrat/column-components'
+import Column, {IUnit} from './column'
+import patterns from '../../geologic-patterns/*.png'
+
+const renderResults = (data: Array<IUnit>)=> {
+  return h(Column, {data});
+};
+
+const ColumnView = (props)=> {
+  const {params} = props
+  // 495
+  return h(APIResultView, {
+    route: "/units",
+    params: {all: true, ...params, response: 'long'}
+  }, renderResults);
+};
+
+const ColumnTitle = (props)=>{
+  return h.if(props.data != null)('h1', props.data?.col_name)
+}
+
+const ColumnManager = ()=> {
+
+  const defaultArgs = {col_id: 2138, project_id: 10, status_code: "in process"}
+  const initArgs = getQueryString() ?? defaultArgs
+  const [columnArgs, setColumnArgs] = useState(initArgs)
+
+  const colParams = {...columnArgs, format: 'geojson'}
+  const res = useAPIResult('/columns', colParams, [columnArgs])
+  const columnFeature = res?.features[0]
+
+  return h("div.column-ui",[
+    h("div.column-view", [
+      h(ColumnTitle, {data: columnFeature?.properties}),
+      h(ColumnView, {params: columnArgs})
+    ]),
+    h("div.measurements", [
+      h(APIResultView, {route: "/measurements", params: {
+        ...columnArgs,
+        show_values: true,
+        response: 'long'
+      }})
+    ])
+  ])
+};
+
+const resolvePattern = (id)=>patterns[id]
+
+const App = => {
+  return h(GeologicPatternProvider, {resolvePattern}, (
+    h(APIProvider, {
+      baseURL: "https://dev.macrostrat.org/api/v2",
+      unwrapResponse: (res)=>res.success.data
+    }, h(ColumnManager))
+  )
+}
+
+export default App
