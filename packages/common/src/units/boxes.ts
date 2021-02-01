0c1e35fba74f88dddc0320005f640febb3e61519
Daven Quinn <dev@davenquinn.com>
2021-02-01 17:04:06 -0600

    Moved component to column-components

--- packages/common/src/units/boxes.ts
+++ packages/common/src/units/boxes.ts
@@ -1,11 +1,13 @@
 import h from "@macrostrat/hyper"
-import { useContext, ReactNode, useRef, useEffect, useState } from "react"
+import { useContext, ReactNode } from "react"
 import {
   ColumnContext,
   ColumnLayoutContext,
   PatternDefsProvider,
   useGeologicPattern,
   ForeignObject,
+  SizeAwareLabel,
+  SizeAwareLabelProps,
 } from "@macrostrat/column-components"
 import { IUnit } from "./types"
 import { resolveID, scalePattern } from "./resolvers"
@@ -39,63 +41,6 @@ const Unit = (props: UnitProps) => {
     },
     children,
   })
-}
-
-interface ElementSize {
-  width: number
-  height: number
-}
-
-function refSize(ref: React.RefObject<HTMLElement>): ElementSize {
-  const { width, height } = ref.current?.getBoundingClientRect()
-  return { width, height }
-}
-
-type SizeAwareLabelProps = React.HTMLProps<"div"> & {
-  label: React.ReactNode
-  isShown?: boolean
-  onVisibilityChanged?(fits: boolean): void
-}
-
-function SizeAwareLabel(props: SizeAwareLabelProps) {
-  /** A label that only renders if it fits within its container div.
-   * This helps build unit and interval labels that do sensible things
-   * when.
-   *
-   * You can use the onUpdate and isShown props to make this a "controlled"
-   * component, or you can use the internally managed state. `onUpdate` can
-   * be used by itself when you need to report back whether the label was
-   * rendered or not (e.g., so you can render it in another location).
-   */
-  const { label, isShown, onVisibilityChanged, ...rest } = props
-  const containerRef = useRef<HTMLElement>()
-  const labelRef = useRef<HTMLElement>()
-  const [fits, setFits] = useState<boolean | null>(null)
-  useEffect(() => {
-    const containerSz = refSize(containerRef)
-    const labelSz = refSize(labelRef)
-    const doesFit =
-      labelSz.width <= containerSz.width && labelSz.height <= containerSz.height
-    setFits(doesFit)
-  }, [containerRef, labelRef])
-
-  // Report whether label fits upwards, if needed
-  useEffect(() => {
-    if (fits == null) return
-    onVisibilityChanged?.(fits)
-  }, [fits])
-
-  const shouldShow = isShown ?? fits ?? true
-
-  return h(
-    "div.unit-overlay",
-    { ...rest, ref: containerRef },
-    h(
-      "span.unit-label",
-      { ref: labelRef, style: { visibility: shouldShow ? null : "hidden" } },
-      label
-    )
-  )
 }
 
 interface LabeledUnitProps extends SizeAwareLabelProps {
