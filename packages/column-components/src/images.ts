1fc097974e5ff7d2155637460ae6724f386b1b7e
Daven Quinn <dev@davenquinn.com>
2021-05-05 04:01:12 -0500

    Updated column image component

--- packages/column-components/src/images.ts
+++ packages/column-components/src/images.ts
@@ -1,25 +1,13 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS206: Consider reworking classes to avoid initClass
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-import { Component } from "react";
+import { useContext } from "react";
 import h from "./hyper";
 import { ColumnContext } from "./context";
 
-class ColumnImage extends Component {
-  static initClass() {
-    this.contextType = ColumnContext;
-  }
-  render() {
-    const { src, ...rest } = this.props;
-    const { pixelHeight } = this.context;
-    return h("div.column-image", { style: rest }, [
-      h("img", { src, style: { height: pixelHeight } }),
-    ]);
-  }
+function ColumnImage(props) {
+  const { src, ...rest } = props;
+  const { pixelHeight } = useContext(ColumnContext);
+  return h("div.column-image", { style: rest }, [
+    h("img", { src, style: { height: pixelHeight } }),
+  ]);
 }
-ColumnImage.initClass();
 
 export { ColumnImage };
