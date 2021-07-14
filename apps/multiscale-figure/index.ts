a132abfef6455462e5b6c8db0ab9f9c8a5e9aec1
Daven Quinn <dev@davenquinn.com>
2021-07-14 00:39:27 -0600

    Make something render

--- apps/multiscale-figure/index.ts
+++ apps/multiscale-figure/index.ts
@@ -1,68 +1,15 @@
-import h, { C, compose } from "@macrostrat/hyper"
-import { APIProvider, useAPIResult } from "@macrostrat/ui-components"
-import { GeologicPatternProvider } from "@macrostrat/column-components"
-import Column from "../carbon-isotopes/column"
-import { ColumnMapNavigator, MeasurementsLayer } from "common/column-map"
-import { MeasurementDataProvider } from "../carbon-isotopes/data-provider"
-import patterns from "url:../../geologic-patterns/*.png"
-import { useColumnNav } from "common/macrostrat-columns"
+import "core-js/stable"
+import "regenerator-runtime/runtime"
 
-const ColumnTitle = props => {
-  return h.if(props.data != null)("h1", props.data?.col_name)
-}
+import { FocusStyleManager } from "@blueprintjs/core"
+import "@blueprintjs/core/lib/css/blueprint.css"
+import "@macrostrat/column-components/dist/esm/index.css"
+import "@macrostrat/map-components/dist/esm/index.css"
 
-const defaultArgs = {
-  col_id: 2192,
-  project_id: 10,
-  status_code: "in process",
-}
+FocusStyleManager.onlyShowFocusOnTabs()
 
-const ColumnManager = () => {
-  const [columnArgs, setCurrentColumn] = useColumnNav(defaultArgs)
+import { render } from "react-dom"
+import h from "@macrostrat/hyper"
+import App from "./app"
 
-  const { col_id, ...projectParams } = columnArgs
-
-  const colParams = { ...columnArgs, format: "geojson" }
-  const res = useAPIResult("/columns", colParams, [columnArgs])
-  const columnFeature = res?.features[0]
-
-  return h(MeasurementDataProvider, columnArgs, [
-    h("div.column-ui", [
-      h("div.column-view", [
-        h(ColumnTitle, { data: columnFeature?.properties }),
-        h(Column, { params: columnArgs }),
-      ]),
-      h("div.map-column", [
-        h(
-          ColumnMapNavigator,
-          {
-            currentColumn: columnFeature,
-            setCurrentColumn,
-            margin: 0,
-            ...projectParams,
-          },
-          h(MeasurementsLayer, {
-            ...projectParams,
-            style: {
-              fill: "dodgerblue",
-              stroke: "blue",
-            },
-          })
-        ),
-      ]),
-    ]),
-  ])
-}
-
-const resolvePattern = id => patterns[id]
-
-const App = compose(
-  C(GeologicPatternProvider, { resolvePattern }),
-  C(APIProvider, {
-    baseURL: "https://dev.macrostrat.org/api/v2",
-    unwrapResponse: res => res.success.data,
-  }),
-  ColumnManager
-)
-
-export default App
+render(h(App), document.querySelector("#app"))
