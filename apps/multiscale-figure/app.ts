39b506d8596cd56730447543132976eeca3336b8
Daven Quinn <dev@davenquinn.com>
2021-07-16 03:20:22 -0600

    Updated multiscale column

--- apps/multiscale-figure/app.ts
+++ apps/multiscale-figure/app.ts
@@ -2,43 +2,23 @@ import h, { C, compose } from "@macrostrat/hyper"
 import { APIProvider, useAPIResult } from "@macrostrat/ui-components"
 import { GeologicPatternProvider } from "@macrostrat/column-components"
 import Column from "../carbon-isotopes/column"
-import { ColumnMapNavigator, MeasurementsLayer } from "common/column-map"
 import { MeasurementDataProvider } from "../carbon-isotopes/data-provider"
 import { MacrostratMeasurementProvider } from "./data-providers"
 import { useColumnNav } from "common/macrostrat-columns"
-import { ColumnMapNavigator } from "common/column-map"
-import Column2 from "../enriched-timeline/column"
 import patterns from "url:../../geologic-patterns/*.png"
 
 const ColumnTitle = props => {
   return h.if(props.data != null)("h1", props.data?.col_name)
 }
 
-const defaultArgs = {
+const columnArgs = {
   col_id: 2163,
   project_id: 10,
   status_code: "in process",
 }
 
-const ColumnView = props => {
-  const { params } = props
-  const data = useAPIResult("/units", {
-    all: true,
-    ...params,
-    response: "long",
-  })
-  if (data == null) return null
-  return h(Column2, { data })
-}
-
 const ColumnManager = () => {
-  const [columnArgs, setCurrentColumn] = useColumnNav(defaultArgs)
-
   const { col_id, ...projectParams } = columnArgs
-
-  const colParams = { ...columnArgs, format: "geojson" }
-  const res = useAPIResult("/columns", colParams, [columnArgs])
-  const columnFeature = res?.features[0]
 
   const params1 = { col_id: 1481 }
   return h("div.column-ui", [
@@ -50,7 +30,6 @@ const ColumnManager = () => {
       ),
     ]),
     h("div.column-view", [
-      h(ColumnTitle, { data: columnFeature?.properties }),
       h(MeasurementDataProvider, columnArgs, [
         h(Column, { params: columnArgs }),
       ]),
