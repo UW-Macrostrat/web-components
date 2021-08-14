becc49f5295b04ad81f4e048c209e0a88a9e8311
Daven Quinn <dev@davenquinn.com>
2021-08-14 07:09:05 -0400

    Added two identical paleogeography applets

--- deploy/deploy-steno.sh
+++ deploy/deploy-steno.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-build_dir=./dist/steno/
+build_dir=./dist/
 public_url=/next/ui-concepts/
 
 parcel build \
-  --out-dir $build_dir \
+  --dist-dir $build_dir \
   --public-url $public_url \
-  apps/*/index.html apps/index.html
+  apps/index.html
 
 rsync -azv --delete $build_dir steno:/data/projects/macrostrat$public_url
