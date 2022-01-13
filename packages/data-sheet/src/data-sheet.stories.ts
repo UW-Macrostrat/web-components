ea0ee71ffa12a302cba79c3fbae776620efdc13d
Daven Quinn <dev@davenquinn.com>
2022-01-13 11:11:30 -0500

    Added a non-functional virtualized data sheet story

--- packages/data-sheet/src/data-sheet.stories.ts
+++ packages/data-sheet/src/data-sheet.stories.ts
@@ -1,7 +1,8 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { SaveButton } from "@macrostrat/ui-components";
+import { ErrorBoundary } from "@macrostrat/ui-components";
 import h from "@macrostrat/hyper";
 import { DataArea, orientationFields } from "./test-sheet";
+import { VirtualizedSheet } from "./virtualized";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -9,7 +10,7 @@ export default {
   component: DataArea,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
-} as ComponentMeta<typeof SaveButton>;
+} as ComponentMeta<typeof DataArea>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 
@@ -20,7 +21,6 @@ function transformData(data: object): GridElement[] {
 }
 
 const Template: ComponentStory<typeof DataArea> = ({ data }) => {
-  console.log(data);
   return h(DataArea, { data: data.map(transformData) });
 };
 
@@ -45,4 +45,40 @@ Primary.args = {
     },
   ],
 };
+
+const Template1: ComponentStory<typeof DataArea> = ({ data }) => {
+  return h(
+    ErrorBoundary,
+    null,
+    h(VirtualizedSheet, {
+      width: 500,
+      height: 500,
+      data: data.map(transformData),
+      valueRenderer: (d) => d.value,
+    })
+  );
+};
+
+export const Virtualized = Template1.bind({});
+Virtualized.args = {
+  data: [
+    {
+      strike: 10,
+      dip: 8,
+      rake: 2,
+      maxError: 20,
+      minError: 8,
+      color: "#65499e",
+    },
+    {
+      strike: 120,
+      dip: 46,
+      rake: 5,
+      maxError: 45,
+      minError: 2,
+      color: "dodgerblue",
+    },
+  ],
+};
+
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
