ea994bcff038a27fd2081adf8dc4ab5fd9df702e
Casey Idzikowski <idzikoj1234@gmail.com>
2022-04-14 15:33:30 +0200

    unused imports

--- packages/ui-components/src/hierarchy/index.ts
+++ packages/ui-components/src/hierarchy/index.ts
@@ -1,7 +1,5 @@
-import { fetchStratNames } from "./fetch";
 import { hyperStyled } from "@macrostrat/hyper";
 import styles from "./hierarchy.module.scss";
-import { useEffect } from "react";
 
 const h = hyperStyled(styles);
 
