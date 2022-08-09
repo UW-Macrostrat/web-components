ff06fbe56f775d087e983b21cff3089117150920
Daven Quinn <dev@davenquinn.com>
2022-08-08 23:58:44 -0500

    Fixed scss|sass typings

---
+++ packages/ui-components/src/typings.d.ts
@@ -0,0 +1,15 @@
+declare module "*.module.scss" {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
+declare module "*.module.sass" {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
