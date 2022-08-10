0fee4e9063e4ce7343320391d358cbe100c44737
Daven Quinn <dev@davenquinn.com>
2022-08-10 17:38:04 -0500

    Added function for map labels

--- packages/mapbox-utils/src/position.ts
+++ packages/mapbox-utils/src/position.ts
@@ -56,3 +56,16 @@ export function setMapPosition(map: Map, pos: MapPosition) {
     map.setFreeCameraOptions(cameraOptions);
   }
 }
+
+
+export function toggleMapLabelVisibility(map: Map, visible: boolean) {
+  // Disable labels on the map
+  console.log("Toggling map visibility");
+  for (let lyr of map.getStyle().layers) {
+    if (!("layout" in lyr)) continue;
+    const isLabelLayer = lyr.layout["text-field"] != null;
+    if (isLabelLayer) {
+      map.setLayoutProperty(lyr.id, "visibility", visible ? "visible" : "none");
+    }
+  }
+}
