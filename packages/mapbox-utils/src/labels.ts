5a7ab48cfafae2bdfbbf5586970fbf3d4732c548
Daven Quinn <dev@davenquinn.com>
2022-08-17 01:50:19 -0500

    Allow omitting layers from map label toggling

--- packages/mapbox-utils/src/labels.ts
+++ packages/mapbox-utils/src/labels.ts
@@ -1,12 +1,15 @@
 import { Map } from "mapbox-gl";
 
-export function toggleMapLabelVisibility(map: Map, visible: boolean) {
+export function toggleMapLabelVisibility(
+  map: Map,
+  visible: boolean,
+  omitLayers: string[] = []
+) {
   // Disable labels on the map
-  console.log("Toggling map visibility");
   for (let lyr of map.getStyle().layers) {
     if (!("layout" in lyr)) continue;
     const isLabelLayer = lyr.layout["text-field"] != null;
-    if (isLabelLayer) {
+    if (isLabelLayer && !omitLayers.includes(lyr.id)) {
       map.setLayoutProperty(lyr.id, "visibility", visible ? "visible" : "none");
     }
   }
