d93f9b01adb69ca3c9a1fdefcf456a818577d326
Daven Quinn <dev@davenquinn.com>
2023-04-16 16:22:27 -0500

    Updated storybook details

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# `[2.0.0]` - 2023-04-16
+
+- Internalized all modules in the monorepo, instead of referencing as submodules.
+- Upgraded to Storybook 7.0.5
+- Removed Stylus in favor of SCSS in all modules
--- storybook/.storybook/theme.js
+++ storybook/.storybook/theme.js
@@ -2,7 +2,7 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "light",
-  brandTitle: `<img src="https://macrostrat.org/img/logo_red.png" width="16px" height="16px"/>`,
+  brandTitle: `<img src="https://macrostrat.org/img/logo_red.png" width="16px" height="16px"/>&nbsp;Macrostrat`,
   brandUrl: "https://macrostrat.org",
   brandTarget: "_self",
 });
