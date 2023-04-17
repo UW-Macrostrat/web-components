9e2fdea6698b8eae79e9e99391c00a7a9645e5e7
Daven Quinn <dev@davenquinn.com>
2023-04-17 17:25:26 -0500

    Added some details for location panel

--- packages/map-interface/src/index.ts
+++ packages/map-interface/src/index.ts
@@ -1,5 +1,7 @@
 import h from "@macrostrat/hyper";
 
+export * from "./location-panel";
+
 export function MapInterface() {
   return h("div", "Hello world");
 }
---
+++ packages/map-interface/src/location-panel/index.ts
@@ -0,0 +1,22 @@
+import { Card } from "@blueprintjs/core";
+import hyper from "@macrostrat/hyper";
+import { InfoDrawerHeader } from "./header";
+import classNames from "classnames";
+import styles from "./main.module.sass";
+import { ErrorBoundary } from "@macrostrat/ui-components";
+
+const h = hyper.styled(styles);
+
+export function InfoDrawerContainer(props) {
+  const className = classNames("infodrawer", props.className);
+  return h(Card, { ...props, className });
+}
+
+export function LocationPanel(props) {
+  const { children, className, loading = false, ...rest } = props;
+  const cls = classNames("location-panel", className, { loading });
+  return h(InfoDrawerContainer, { className: cls }, [
+    h(InfoDrawerHeader, rest),
+    h("div.infodrawer-body", [h(ErrorBoundary, null, children)]),
+  ]);
+}
