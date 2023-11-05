a4a4935fd40f3be8562739a49bdef58d45e73b6d
Daven Quinn <dev@davenquinn.com>
2023-11-05 12:33:19 -0600

    Started changing editors

--- packages/data-sheet/src/editors/index.ts
+++ packages/data-sheet/src/editors/index.ts
@@ -1,5 +1,4 @@
 import h from "@macrostrat/hyper";
-import { DataEditor } from "react-datasheet/lib";
 import { ErrorBoundary } from "@macrostrat/ui-components";
 import { SketchPicker } from "react-color";
 import { Popover2 } from "@blueprintjs/popover2";
@@ -8,66 +7,96 @@ import chroma from "chroma-js";
 import "./styles.module.scss";
 
 export function BasePopupEditor(props) {
-  const { children, ...rest } = props;
+  const { children, valueRenderer, value, ...rest } = props;
 
-  return h([
-    h(DataEditor, rest),
-    h("span.popover-container", [
-      h(
-        Popover2,
+  return h(
+    Popover2,
+    {
+      content: h(
+        "div.interaction-barrier",
         {
-          content: h(
-            "div.interaction-barrier",
-            {
-              onMouseDown(evt) {
-                evt.nativeEvent.stopImmediatePropagation();
-              },
-              onKeyDown(evt) {
-                console.log(evt);
-              },
-            },
-            h(ErrorBoundary, null, children)
-          ),
-          enforceFocus: false,
-          autoFocus: false,
-          minimal: true,
-          modifiers: {
-            offset: { enabled: true, options: { offset: [0, 8] } },
+          onMouseDown(evt) {
+            evt.nativeEvent.stopImmediatePropagation();
           },
-          interactionKind: "hover-target",
-          isOpen: true,
-          onClose(evt) {
-            props.onKeyDown(evt);
+          onKeyDown(evt) {
+            console.log(evt);
           },
-          usePortal: false,
         },
-        h("span.popover-target")
+        h(ErrorBoundary, null, children)
       ),
-    ]),
-  ]);
+      enforceFocus: false,
+      autoFocus: false,
+      minimal: true,
+      modifiers: {
+        //offset: { enabled: true, options: { offset: [0, 8] } },
+      },
+      interactionKind: "hover-target",
+      isOpen: true,
+      onClose(evt) {
+        props.onKeyDown(evt);
+      },
+      usePortal: false,
+    },
+    children
+  );
+}
+
+export function ColorPicker({ value, onChange }) {
+  let color = "#aaaaaa";
+  try {
+    color = chroma(value).hex();
+  } catch {}
+  return h(SketchPicker, {
+    disableAlpha: true,
+    color,
+    onChange(color, evt) {
+      let c = "";
+      try {
+        c = chroma(color.hex).name();
+      } finally {
+        onChange(c);
+        evt.stopPropagation();
+      }
+    },
+  });
 }
 
 export function ColorEditor(props) {
   const { value, onChange, ...rest } = props;
-  let color = null;
-  try {
-    color = chroma(value).hex();
-  } catch {}
+  return h(BasePopupEditor, rest, h(ColorPicker, { value, onChange }));
+}
+
+export function EditorPopup(props) {
+  const { children, content } = props;
+
   return h(
-    BasePopupEditor,
-    rest,
-    h(SketchPicker, {
-      disableAlpha: true,
-      color: color ?? "#aaaaaa",
-      onChange(color, evt) {
-        let c = "";
-        try {
-          c = chroma(color.hex).name();
-        } finally {
-          onChange(c);
-          evt.stopPropagation();
-        }
+    Popover2,
+    {
+      content: h(
+        "div.interaction-barrier",
+        {
+          onMouseDown(evt) {
+            evt.nativeEvent.stopImmediatePropagation();
+          },
+          onKeyDown(evt) {
+            console.log(evt);
+          },
+        },
+        h(ErrorBoundary, null, content)
+      ),
+      enforceFocus: false,
+      autoFocus: false,
+      minimal: true,
+      modifiers: {
+        offset: { enabled: true, options: { offset: [0, 8] } },
       },
-    })
+      interactionKind: "hover-target",
+      isOpen: true,
+      onClose(evt) {
+        props.onKeyDown(evt);
+      },
+      usePortal: true,
+    },
+    children
   );
 }
