f0886819f55c8f49ae2b1c4487000ac24f08cad2
Daven Quinn <code@davenquinn.com>
2024-03-25 14:58:55 -0500

    Made feedback component a bit more fully functional

--- packages/feedback-components/src/index.ts
+++ packages/feedback-components/src/index.ts
@@ -1,9 +1,23 @@
 import h from "@macrostrat/hyper";
+import { Annotator } from "poplar-annotation";
+import { useRef, useEffect } from "react";
 
 export interface FeedbackComponentProps {
   // Add props here
 }
 
-export function FeedbackComponent(props: FeedbackComponentProps) {
-  return h("div", { className: "feedback-component" }, "FeedbackComponent");
+export function FeedbackComponent() {
+  const annotator = useRef<Annotator | null>(null);
+  const container = useRef<HTMLDivElement | null>();
+
+  useEffect(() => {
+    if (container.current == null) return;
+    annotator.current = new Annotator(
+      "Hello world fffffffff",
+      container.current,
+      {}
+    );
+  }, [container.current]);
+
+  return h("div", { className: "feedback-component", ref: container });
 }
