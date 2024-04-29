9ded91c282cb631a2c04566a926f43d612def743
dsarda <dsarda@cosmos0003.chtc.wisc.edu>
2024-04-29 12:20:25 -0500

    Added single example of updated feedback component

---
+++ packages/feedback-components/src/Node.tsx
@@ -0,0 +1,58 @@
+import { AiFillFolder, AiFillFile } from "react-icons/ai";
+import { MdArrowRight, MdArrowDropDown, MdEdit } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+import { NodeApi, Tree, TreeApi } from "react-arborist";
+import React from "react";
+import {Entity, Child, Result, TreeData} from './types';
+
+function isSelected(search_node : TreeData, tree_node: TreeData) {
+  if(search_node.id == tree_node.id) {
+    return true;
+  }
+
+  for(var child of tree_node.children) {
+    if(isSelected(search_node, child)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function isNodeSelected(node : NodeApi<TreeData>, tree: TreeApi<TreeData>) {
+  for(var selected_node of tree.selectedNodes) {
+    if(isSelected(node.data, selected_node.data)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+type COLOR_TYPE = {
+  [key: string]: string;
+  0: string;
+  1: string;
+  2: string;
+};
+
+const COLORS: COLOR_TYPE = {
+  0: "rgb(179, 245, 66)",
+  1: "#42f5f5",
+  2: "#4b46cd",
+};
+
+const Node = ({ node, style, dragHandle, tree } : any) => {
+  let selected : boolean = isNodeSelected(node, tree);
+  let node_level : string = node.data.id.split("_")[0];
+  let nameStyle = selected ? { backgroundColor: COLORS[node_level] } : {};
+  
+  return (
+    <div style={{ ...style, ...nameStyle }} ref={dragHandle}>
+      {"🍁"}
+      {node.data.name}
+    </div>
+  );
+};
+
+export default Node;
