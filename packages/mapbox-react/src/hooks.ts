3ffe5afc8a819ddef70c3798f42a2a7328c802d2
Daven Quinn <code@davenquinn.com>
2024-05-26 00:30:31 -0500

    Added some hooks to update styles

--- packages/mapbox-react/src/hooks.ts
+++ packages/mapbox-react/src/hooks.ts
@@ -1,16 +1,37 @@
-import { RefObject } from "react";
-import { useEffect } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import { toggleMapLabelVisibility } from "@macrostrat/mapbox-utils";
+import { useMapRef } from "./context";
 
+/** A newer and more flexible version of useMapConditionalStyle */
+export function useMapStyleOperator(
+  operator: (map: mapboxgl.Map) => void,
+  dependencies: any[] = []
+) {
+  const mapRef = useMapRef();
+  const isInitialized = useStyleInitialized();
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map == null) return;
+    if (map.isStyleLoaded()) {
+      operator(map);
+    }
+    const fn = () => operator(map);
+    return fn();
+  }, [mapRef.current, isInitialized, ...dependencies]);
+}
+
+/**
+ * Apply conditional style logic depending on a state value or object.
+ * The operator function can operate on the Map and the state, and is applied on state changes
+ * and on style load events.
+ * @deprecated Use useMapStyleCallback instead
+ */
 export function useMapConditionalStyle<T = any>(
   mapRef: RefObject<mapboxgl.Map>,
   state: T,
   operator: (map: mapboxgl.Map, a: T) => void
 ) {
-  /** Apply conditional style logic depending on a state value or object.
-   * The operator function can operate on the Map and the state, and is applied on state changes
-   * and on style load events. */
   useEffect(() => {
     const map = mapRef.current;
     if (map == null) return;
@@ -38,4 +59,31 @@ export function useMapLabelVisibility(
     { mapShowLabels, omitLayers },
     _toggleMapLabels
   );
+}
+
+/**  This function tracks whether a style has been loaded.
+ * This works around the fact that the isStyleLoaded method
+ * sometimes returns false even when the style has been loaded,
+ * resulting in the isStyleLoaded event not being fired.
+ *
+ * This may be related to the issue described here, which has
+ * apparently been fixed in recent Mapbox GL JS versions:
+ * https://github.com/mapbox/mapbox-gl-js/issues/8691
+ */
+export function useStyleInitialized() {
+  const mapRef = useMapRef();
+  const isInitialized = useRef<Boolean>();
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map == null) return;
+    if (map.isStyleLoaded()) {
+      isInitialized.current = true;
+    }
+    const fn = () => (isInitialized.current = true);
+    map.on("style.load", fn);
+    return () => {
+      map.off("style.load", fn);
+    };
+  }, [mapRef.current]);
+  return isInitialized.current;
 }
