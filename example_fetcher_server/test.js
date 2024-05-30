c16da18374c50c0417b1e779fcccb898009ef552
Daven Quinn <code@davenquinn.com>
2024-05-29 22:30:26 -0500

    Remove out-of-place example server

--- example_fetcher_server/test.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:3001").then(response => {
-    console.log("Okay of", response.ok);
-}).catch(error => {
-    console.error("Fetch returned error", error);
-})
--- example_fetcher_server/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-def main():
-    result = requests.get("http://localhost:3001")
-    print(result.json())
-
-if __name__ == "__main__":
-    main()
