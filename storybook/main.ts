3ff08bfcf42af8d1dff5837b8cea555ac4e9aaae
Daven Quinn <code@davenquinn.com>
2024-10-22 01:25:02 -0500

    Updated storybook

--- storybook/main.ts
+++ storybook/main.ts
@@ -1,26 +1,29 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   // vite
-  stories: [
-    "../packages/**/*.stories.@(mdx|js|jsx|ts|tsx)",
-  ],
+  stories: ["../packages/**/*.stories.@(mdx|js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-viewport",
-    "storybook-dark-mode",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-viewport"),
+    getAbsolutePath("storybook-dark-mode"),
   ],
   core: {
     builder: {
-      name: '@storybook/builder-vite',
+      name: getAbsolutePath("@storybook/builder-vite"),
       options: {
         viteConfigPath: "./vite.config.ts",
-      }
-    }
+      },
+    },
   },
-  framework:  "@storybook/react-vite",
+  framework: getAbsolutePath("@storybook/react-vite"),
   docs: {},
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
--- storybook/vite.config.ts
+++ storybook/vite.config.ts
@@ -2,4 +2,8 @@ export default {
   resolve: {
     conditions: ["typescript"],
   },
+  // https://vite.dev/config/shared-options.html#css-preprocessoroptions
+  scss: {
+    api: "modern-compiler", // or "modern", "legacy"
+  },
 };
