80872e54319d5cac2ddd849d94141f8def7112e0
Daven Quinn <code@davenquinn.com>
2024-12-17 15:47:06 -0600

    Addded feedback files from Macrostrat web

---
+++ packages/feedback-components/src/extractions/types.ts
@@ -0,0 +1,29 @@
+type EntityType = string;
+type Match = any;
+
+export interface Entity {
+  id: number;
+  name: string;
+  type?: number;
+  indices: [number, number];
+  children: Entity[];
+  match?: Match;
+}
+
+export { EntityType };
+
+export type Highlight = {
+  start: number;
+  end: number;
+  tag?: string;
+  text?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  id: number;
+  parents?: number[];
+};
+
+export interface EntityExt extends Omit<Entity, "type" | "children"> {
+  type: EntityType;
+  children: EntityExt[];
+}
--- packages/feedback-components/src/index.ts
+++ packages/feedback-components/src/index.ts
@@ -1,24 +1,2 @@
-import hyper from "@macrostrat/hyper";
-import { Annotator } from "poplar-annotation";
-import { useRef, useEffect } from "react";
-import FeedbackWrap from "./FeedbackWrap";
-import styles from "./feedback.module.sass";
-const h = hyper.styled(styles);
-
-export interface FeedbackComponentProps {
-  // Add props here
-}
-
-export function FeedbackComponent({ data }) {
-  const annotator = useRef<FeedbackWrap | null>(null);
-  const ref = useRef<HTMLDivElement | null>();
-
-  useEffect(() => {
-    if (ref.current == null) return;
-    annotator.current = new FeedbackWrap();
-  }, [ref.current, data]);
-
-  return h("div.feedback-component", { ref });
-}
-
-export { FeedbackWrap };
+export * from "./extractions";
+export * from "./feedback";
