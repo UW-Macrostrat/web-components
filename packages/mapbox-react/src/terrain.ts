0ec965721faf7f05f42d5e6fccf3301e554ed3ed
Daven Quinn <code@davenquinn.com>
2025-02-23 01:11:03 -0600

    Break out default layers

--- packages/mapbox-react/src/terrain.ts
+++ packages/mapbox-react/src/terrain.ts
@@ -60,7 +60,8 @@ function setup3DTerrainLegacy(
   shouldEnable: boolean,
   sourceID: string | null = null
 ) {
-  const currentTerrainSource = getTerrainSourceID(map);
+  const style = map.getStyle();
+  const currentTerrainSource = getTerrainSourceID(style);
   let demSourceID = sourceID ?? currentTerrainSource ?? "mapbox-dem";
 
   console.log("Enabling 3D terrain with source", demSourceID);
@@ -76,33 +77,19 @@ function setup3DTerrainLegacy(
 
   // Add a DEM source if one is not found already.
   if (map.getSource(demSourceID) == null) {
-    map.addSource(demSourceID, {
-      type: "raster-dem",
-      url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-      tileSize: 512,
-      maxzoom: 14,
-    });
+    map.addSource(demSourceID, defaultRasterDEM);
   }
 
   // add a sky layer that will show when the map is highly pitched
   if (map.getLayer("sky") == null) {
-    map.addLayer({
-      id: "sky",
-      type: "sky",
-      paint: {
-        "sky-type": "atmosphere",
-        "sky-atmosphere-sun": [0.0, 0.0],
-        "sky-atmosphere-sun-intensity": 15,
-      },
-    });
+    map.addLayer(defaultSkyLayer);
   }
 
   map.setTerrain({ source: demSourceID, exaggeration: 1 });
-  console.log(map.getTerrain());
 }
 
-function getTerrainSourceID(map) {
-  for (const [key, source] of Object.entries(map.getStyle().sources)) {
+function getTerrainSourceID(style: Style): string | null {
+  for (const [key, source] of Object.entries(style.sources)) {
     if (source.type == "raster-dem") {
       return key;
     }
@@ -128,40 +115,48 @@ function addDefault3DStyles(
 
   if (!hasTerrain) {
     map.addSource(sourceName, {
-      type: "raster-dem",
-      url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-      tileSize: 512,
-      maxzoom: 14,
+      ...defaultRasterDEM,
       ...sourceCfg,
     });
   }
 
   if (!hasSky) {
-    map.addLayer({
-      id: "sky",
-      type: "sky",
-      paint: {
-        "sky-type": "atmosphere",
-        "sky-atmosphere-sun": [0.0, 0.0],
-        "sky-atmosphere-sun-intensity": 15,
-      },
-    });
+    map.addLayer(defaultSkyLayer);
   }
   if (map.getFog() == null) {
-    map.setFog({
-      color: "#ffffff",
-      // @ts-ignore
-      "space-color": [
-        "interpolate",
-        ["linear"],
-        ["zoom"],
-        4,
-        "hsl(215, 28%, 64%)",
-        7,
-        "hsl(209, 92%, 85%)",
-      ],
-      "star-intensity": ["interpolate", ["linear"], ["zoom"], 5, 0.35, 6, 0],
-      range: [5, 15],
-    });
+    map.setFog(defaultFog);
   }
 }
+
+const defaultRasterDEM = {
+  type: "raster-dem",
+  url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+  tileSize: 512,
+  maxzoom: 14,
+};
+
+const defaultSkyLayer = {
+  id: "sky",
+  type: "sky",
+  paint: {
+    "sky-type": "atmosphere",
+    "sky-atmosphere-sun": [0.0, 0.0],
+    "sky-atmosphere-sun-intensity": 15,
+  },
+};
+
+const defaultFog = {
+  color: "#ffffff",
+  // @ts-ignore
+  "space-color": [
+    "interpolate",
+    ["linear"],
+    ["zoom"],
+    4,
+    "hsl(215, 28%, 64%)",
+    7,
+    "hsl(209, 92%, 85%)",
+  ],
+  "star-intensity": ["interpolate", ["linear"], ["zoom"], 5, 0.35, 6, 0],
+  range: [5, 15],
+};
