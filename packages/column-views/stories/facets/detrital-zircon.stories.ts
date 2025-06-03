140c7212674d88c8b8469f132f074c4895365f08
davidsklar99 <davidsklar99@outlook.com>
2025-06-03 13:15:46 -0500

    Detrial Zircon Compilation uses ColumnNavigationMap

--- packages/column-views/stories/facets/detrital-zircon.stories.ts
+++ packages/column-views/stories/facets/detrital-zircon.stories.ts
@@ -2,6 +2,7 @@ import {
   ColoredUnitComponent,
   MacrostratDataProvider,
   MeasurementDataProvider,
+  ColumnNavigationMap,
   ColumnNavigationSVGMap,
   MeasurementsLayer,
   useColumnNav,
@@ -10,8 +11,9 @@ import {
 import h from "@macrostrat/hyper";
 import { StandaloneColumn } from "../column-ui";
 import { FlexRow, useAPIResult } from "@macrostrat/ui-components";
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { FeatureCollection } from "geojson";
+import { setGeoJSON } from "@macrostrat/mapbox-utils";
 
 function DetritalZirconColumn(props) {
   const { id, children, spectraColor, ...rest } = props;
@@ -67,40 +69,53 @@ export const BighornBasinColored = {
 };
 
 export function DetritalZirconCompilation(defaultArgs) {
+  const [mapInstance, setMapInstance] = useState(null);
   const [columnArgs, setCurrentColumn] = useColumnNav({
     ...(defaultArgs ?? {}),
     col_id: 495,
   });
 
-  const colParams = useMemo(
-    () => ({ ...columnArgs, format: "geojson" }),
-    [columnArgs]
-  );
-  const res: FeatureCollection = useAPIResult(
-    "https://macrostrat.org/api/v2/columns",
-    colParams,
-    (res) => res?.success?.data
-  );
-  const columnFeature = res?.features[0];
+  const data = getLayer({
+            measure_phase: "zircon",
+            measurement: "207Pb-206Pb",
+            style: { fill: "purple" },
+          });
+
+  const handleMapLoaded = (map) => {
+    setMapInstance(map);
+  };
+
+  useEffect(() => {
+    if (!mapInstance || !data) return;
+    console.log("Setting measurements layer", data);
+    setGeoJSON(mapInstance, "measurements", data);
+    mapInstance.addLayer({
+      id: "measurement-points",
+      type: "circle",
+      source: "measurements",
+      paint: {
+        "circle-color": "purple",
+        "circle-radius": 3,
+        "circle-opacity": 0.8,
+      },
+    });
+  }, [data, mapInstance]);
 
   return h(
     MacrostratDataProvider,
     h(MeasurementDataProvider, columnArgs, [
       h(FlexRow, { className: "column-ui", margin: "2em", gap: "1em" }, [
-        h(
-          ColumnNavigationSVGMap,
-          {
-            currentColumn: columnFeature,
-            setCurrentColumn,
-            margin: 0,
-            style: { width: 400, height: 500 },
-          },
-          h(MeasurementsLayer, {
-            measure_phase: "zircon",
-            measurement: "207Pb-206Pb",
-            style: { fill: "purple" },
-          })
-        ),
+        h(ColumnNavigationMap, {
+          style: { width: 400, height: 500 },
+          onSelectColumn: (e) => setCurrentColumn({
+            ...(defaultArgs ?? {}),
+            col_id: e
+          }),
+          selectedColumn: columnArgs?.col_id,
+          margin: 0,
+          accessToken: import.meta.env.VITE_MAPBOX_API_TOKEN,
+          onMapLoaded: handleMapLoaded,
+        }),
         h(DetritalZirconColumn, {
           id: columnArgs.col_id,
           showLabelColumn: false,
@@ -108,4 +123,23 @@ export function DetritalZirconCompilation(defaultArgs) {
       ]),
     ])
   );
+}
+
+function getLayer(props) {
+  const defaultStyle = {
+    fill: "rgb(239, 180, 249)",
+    stroke: "magenta",
+  };
+  const { style = defaultStyle, ...params } = props;
+
+  console.log("/measurements", {
+    ...params,
+    format: "geojson",
+    response: "light",
+  })
+  return useAPIResult("https://macrostrat.org/api/v2/measurements", {
+    ...params,
+    format: "geojson",
+    response: "light",
+  })?.success?.data
 }
