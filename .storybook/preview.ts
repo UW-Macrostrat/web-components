355bc5cffeaf22633b63ffc8c365c28255ffefbe
davidsklar99 <davidsklar99@users.noreply.github.com>
2025-07-02 18:53:08 +0000

    Apply formatting changes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -54,9 +54,10 @@ export const parameters = {
 export const decorators = [
   (renderStory) => {
     const isEnabled = useDarkMode();
-    return h(PatternProvider,
+    return h(
+      PatternProvider,
       h(DarkModeProvider, { isEnabled }, renderStory()),
-    )
+    );
   },
 ];
 
