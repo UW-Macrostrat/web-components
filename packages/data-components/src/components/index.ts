ec0d7eec5ea037ed4efae20d3154a0d9ebf581d7
davidsklar99 <davidsklar99@outlook.com>
2025-07-30 11:08:33 -0400

    Update exports and changelog

--- packages/data-components/src/components/index.ts
+++ packages/data-components/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./hierarchy";
 export * from "./prevalent-taxa";
 export * from "./unit-details";
+export * from "./location-details";
---
+++ packages/data-components/src/components/location-details/index.ts
@@ -0,0 +1,5 @@
+export * from "./fossil-collections";
+export * from "./xdd-panel";
+export * from "./reg-strat";
+export * from "./physiography"
+export * from "./macrostrat-linked"
