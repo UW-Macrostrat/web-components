5d03b63a1e00c9123b9c5ee7b2c86ef7c0feccae
davidsklar99 <davidsklar99@outlook.com>
2025-07-30 11:22:22 -0400

    Remove console logs

--- packages/map-interface/src/location-details/fossil-collections/collections.tsx
+++ packages/map-interface/src/location-details/fossil-collections/collections.tsx
@@ -14,7 +14,6 @@ export default function PBDBCollections({ data }) {
 }
 
 function FossilCollection({ col }) {
-  console.log("FossilCollection", col);
   let occurrenceTree = makeOccurrenceTree(col.occurrences);
   return h(
     "div.fossil-collection",
--- packages/map-interface/src/location-details/info-blocks.ts
+++ packages/map-interface/src/location-details/info-blocks.ts
@@ -51,8 +51,6 @@ function Age({ age }) {
 function AgeChip(props) {
   const { t_int, b_int, intervalURL } = props;
 
-  console.log(intervalURL);
-
   return h("div.age-chip-container", [
     h(IntervalChip, { interval: b_int, intervalURL }),
     h.if(b_int.int_id != props.t_int.int_id)(IntervalChip, {
