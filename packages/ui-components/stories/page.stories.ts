8d997820fc92c5a00b4ee83010350a39328739bb
davidsklar99 <davidsklar99@outlook.com>
2025-08-12 13:19:18 -0400

    Paged works

---
+++ packages/ui-components/stories/page.stories.ts
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react-vite";
+import h from "@macrostrat/hyper";
+import { Pagination } from "../src";
+import { useState } from "react"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "UI components/Pagination",
+  component: Pagination,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof Pagination>;
+
+export function Basic() {
+  const [ix, setIX] = useState(0)
+  return h('div', [
+    h('p', `Current page: ${ix}`),
+    h(Pagination, { currentPage: ix, nextDisabled: false, setPage: (page: number) => setIX(page) }),
+  ])
+}
