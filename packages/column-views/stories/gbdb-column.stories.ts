866a2841cf70bc1abf49fe14e4768aba380f209f
Daven Quinn <code@davenquinn.com>
2025-09-24 00:32:16 +0800

    Estimate linear age interpolation

--- packages/column-views/stories/gbdb-column.stories.ts
+++ packages/column-views/stories/gbdb-column.stories.ts
@@ -20,7 +20,7 @@ export default {
   },
   argTypes: {
     axisType: {
-      options: ["age", "height", "depth"],
+      options: ["age", "height"],
       control: { type: "radio" },
     },
   },
@@ -62,11 +62,56 @@ function convert(unit: any): UnitLong {
   };
 }
 
+function stackUnitsByAge(units: UnitLong[]): UnitLong[] {
+  /** Find groups of units with same top and bottom age, and stack them */
+  const used = new Set<number>();
+  const newUnits: UnitLong[] = [];
+  for (let i = 0; i < units.length; i++) {
+    if (used.has(i)) continue;
+    const u1 = units[i];
+    const group = [u1];
+    used.add(i);
+    for (let j = i + 1; j < units.length; j++) {
+      if (used.has(j)) continue;
+      const u2 = units[j];
+      if (u1.t_age === u2.t_age && u1.b_age === u2.b_age) {
+        group.push(u2);
+        used.add(j);
+      }
+    }
+    if (group.length === 1) {
+      newUnits.push(u1);
+    } else {
+      // Stack the units in the group
+      let u0 = group[0];
+      const totalThickness = u0.b_age - u0.t_age;
+      const fracThickness = totalThickness / group.length;
+      let cumulativeThickness = 0;
+      // Sort group by height
+      group.sort((a, b) => (b.t_pos ?? 0) - (a.t_pos ?? 0));
+
+      for (const u of group) {
+        const newTAge = u.t_age + cumulativeThickness;
+        const newBAge = newTAge + fracThickness;
+        cumulativeThickness += fracThickness;
+        newUnits.push({
+          ...u,
+          t_age: newTAge,
+          b_age: newBAge,
+        });
+      }
+    }
+  }
+  return newUnits;
+}
+
 export function GBDBColumn({ axisType = ColumnAxisType.HEIGHT }) {
   const units = useMemo(() => {
     console.log("Column data", data);
 
-    const units: UnitLong[] = data.map(convert).filter((d) => d != null);
+    const units: UnitLong[] = stackUnitsByAge(
+      data.map(convert).filter((d) => d != null),
+    );
 
     console.log("Converted units", units);
     return units;
