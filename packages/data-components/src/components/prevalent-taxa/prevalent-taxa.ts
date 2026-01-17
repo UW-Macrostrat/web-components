9ee2b04729c656ea7d0ccf2a4c06b494fe8193a2
Daven Quinn <code@davenquinn.com>
2026-01-17 16:17:03 -0600

    Update storybook

--- packages/data-components/src/components/prevalent-taxa/prevalent-taxa.ts
+++ packages/data-components/src/components/prevalent-taxa/prevalent-taxa.ts
@@ -4,7 +4,6 @@ import { hyperStyled } from "@macrostrat/hyper";
 //@ts-ignore
 import styles from "./taxa.module.scss";
 import { Spinner } from "@blueprintjs/core";
-import fetch from "node-fetch";
 
 const h = hyperStyled(styles);
 
